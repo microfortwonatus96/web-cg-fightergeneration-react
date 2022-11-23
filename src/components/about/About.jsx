6ab73ab83e465d4f2877ea48eb1c044b123c9c95
Micro Fortwonatus <microfortwonatus6@gmail.com>
2022-11-23 09:53:18 +0700

    setting General Css and Responsive Media Screen

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      dvdv
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      vdvdv
+    </div>
+  )
+}
+
+export default Header

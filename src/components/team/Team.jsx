486b5c6a0dc1cf4c4f8c00a3ae0eaa07c032e024
Micro Fortwonatus <microfortwonatus6@gmail.com>
2022-12-01 05:59:37 +0700

    our-teams responsive

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -8,11 +8,6 @@ const Team = () => {
       <h2 className="section__title">Our Teams</h2>
       <div className="team__container container grid">
         <Profile />
-
-        {/* <p className="team__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          delectus suscipit dolorem mollitia impedit sequi?
-        </p> */}
       </div>
     </section>
   );
